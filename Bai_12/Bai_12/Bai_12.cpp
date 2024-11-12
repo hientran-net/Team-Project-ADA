a8a9d98dd5d6e71e453eb4470e76d4a31c982931
HienTran <hientm0978@gmail.com>
2024-11-12 16:56:43 +0700

    a

---
+++ Bai_12/Bai_12/Bai_12.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <fstream>
+
+using namespace std;
+
+// ham in mang
+void print(ofstream& f, vector<int>& a) {
+    f << "[";
+    for (int i = 0; i < a.size(); i++) {
+        f << a[i];
+        if (i < a.size() - 1) f << ", ";
+    }
+    f << "]";
+}
+
+
+void timNhomToiUu(vector<int>& a, vector<int>& nhom1, vector<int>& nhom2, int& ketQua) {
+
+    // tinh tong cac phan tu trong mang chua N so nguyen
+    int total = accumulate(a.begin(), a.end(), 0);
+
+    // so luong phan tu trong mang
+    int n = a.size();
+
+
+    // khởi tạo mảng ( tổng khả dĩ - tổng có thể ) có kích thước tổng các phần tử + 1, gán tất cả bằng false
+    vector<bool> possible_sums(total + 1, false);
+    
+    // trường hợp luôn đúng
+    possible_sums[0] = true;
+
+
+    for (int num : a) {
+        for (int j = total; j >= num; j--) {
+            if (possible_sums[j - num]) {
+                possible_sums[j] = true;
+            }
+        }
+    }
+    int optimal_sum = total / 2;
+    while (!possible_sums[optimal_sum]) {
+        optimal_sum--;
+    }
+
+    int current_sum = 0;
+    sort(a.rbegin(), a.rend());
+
+    nhom1.clear();
+    nhom2.clear();
+
+    for (int num : a) {
+        if (current_sum + num <= optimal_sum) {
+            nhom1.push_back(num);
+            current_sum += num;
+        }
+        else {
+            nhom2.push_back(num);
+        }
+    }
+    int tong1 = accumulate(nhom1.begin(), nhom1.end(), 0LL);
+    int tong2 = accumulate(nhom2.begin(), nhom2.end(), 0LL);
+    ketQua = tong1 * tong2;
+}
+
+int main() {
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
+
+    if (!fin.is_open()) {
+        cout << "error to open input.txt\n";
+        return 1;
+    }
+    if (!fout.is_open()) {
+        cout << "error to open output.txt\n";
+        return 1;
+    }
+
+    int totalTestCase; // số bộ test
+    fin >> totalTestCase;
+
+    vector<int> nhom1, nhom2;
+    int ketQua;
+
+    while (totalTestCase--) {
+        int n; // số phần tử của mảng
+        fin >> n;
+
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            fin >> a[i];
+        }
+
+        fout << "Day so ban dau: ";
+        print(fout, a);
+        fout << "\n";
+
+        timNhomToiUu(a, nhom1, nhom2, ketQua);
+
+        fout << "Nhom thu nhat: ";
+        print(fout, nhom1);
+        fout << " (tong nhom thu nhat = " << accumulate(nhom1.begin(), nhom1.end(), 0) << ")\n";
+
+        fout << "Nhóm thu hai: ";
+        print(fout, nhom2);
+        fout << " (tong nhom thu hai = " << accumulate(nhom2.begin(), nhom2.end(), 0) << ")\n";
+
+        fout << "Tich cua tong hai nhom: " << ketQua << "\n\n";
+    }
+
+    return 0;
+}
---
+++ Bai_16/tESTpROJECT/tESTpROJECT.cpp
@@ -0,0 +1,130 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+
+using namespace std;
+
+const char* fi = "CHIADAGIAC.INP";
+const char* fo = "CHIADAGIAC.OUT";
+const int MAXN = 100;
+const double oo = 1e9;
+
+int dem = 0;
+int n;
+double a[MAXN + 1], b[MAXN + 1];
+int H[MAXN + 1][MAXN + 1];
+double L[MAXN + 1][MAXN + 1]; // L[i][j] tổng độ dài các đường chéo khi xét từ đỉnh i đến đỉnh j
+double d[MAXN + 1][MAXN + 1]; // d là độ dài đường chéo giữa đỉnh i và j
+
+void Nhap() {
+    ifstream f(fi);
+    if (!f) {
+        cerr << "Error opening file " << fi << endl;
+        exit(1);
+    }
+    f >> n;
+    for (int i = 1; i <= n; i++) {
+        f >> a[i] >> b[i];
+    }
+    f.close();
+}
+
+double KhoangCach(int i, int j) {
+    return sqrt(pow((a[i] - a[j]), 2) + pow((b[i] - b[j]), 2));
+}
+
+void TinhKhoangCachCacDuongCheo() {
+    memset(d,0, sizeof(d));
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j < i; j++) {
+            if (i != j && abs(i - j) != 1) {
+                d[i][j] = KhoangCach(i, j);
+                d[j][i] = d[i][j];
+            }
+        }
+    }
+}
+
+void QHD() {
+    memset(L, 0, sizeof(L));
+
+    for (int m = 3; m <= n - 1; m++) {
+        for (int i = 1; i <= n - m; i++) { // 1 - 4, 1 2 3 4
+            int j = i + m; // 4 - 7
+            L[i][j] = +oo; // + vo cung
+            for (int k = i; k < j; k++) { 
+                double tong = L[i][k] + L[k][j] + d[i][k] + d[k][j];
+                cout << ++dem << " ";
+                if (L[i][j] > tong) {
+                    L[i][j] = tong;
+                    H[i][j] = k;
+                }
+            }
+        }
+    }
+}
+
+void TruyVet(int i, int j, ofstream& f) {
+    if (j - i >= 3) {
+        int k = H[i][j];
+        f << "Noi dinh " << i << " voi dinh " << k << endl;
+        f << "Noi dinh " << k << " voi dinh " << j << endl;
+        TruyVet(i, k, f);
+        TruyVet(k, j, f);
+    }
+}
+
+int main() {
+    Nhap();
+    TinhKhoangCachCacDuongCheo();
+    QHD();
+
+    ofstream f(fo);
+
+
+    f << "Do dai ngan nhat ";
+    f << fixed << setprecision(2) << L[1][n] << endl;
+    TruyVet(1, n, f);
+    f.close();
+
+    cout << "\n"; cout << "\n"; cout << "\n";
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << d[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n"; cout << "\n"; cout << "\n";
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << L[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+
+    cout << "\n"; cout << "\n"; cout << "\n";
+
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << H[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+
+    return 0;
+}
